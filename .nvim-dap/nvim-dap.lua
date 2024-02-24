ad805de33ac001ec21e6c93a17492717c96f4756
Oskari Tuormaa <oskaritu@gmail.com>
2024-02-24 22:08:57 +0100

    Initial example Pico W blinky example

---
+++ .nvim-dap/nvim-dap.lua
@@ -0,0 +1,39 @@
+local dap = require("dap")
+local nmap = require("custom.utils").nmap
+
+nmap("<leader>e", ":sp term://cmake --build build -j30<cr>")
+nmap("<leader>E", ":sp term://bash upload_and_program.sh<cr>")
+
+dap.adapters.cppdbg = {
+    id = 'cppdbg',
+    type = "executable",
+    command = "/home/oskari/dl/cpptools/extension/debugAdapters/bin/OpenDebugAD7"
+}
+
+local exec_name = "n64-sniffer.elf"
+
+dap.configurations.cpp = {
+    {
+        name = "Debug J-Link",
+        type = "cppdbg",
+        request = "launch",
+        MIMode = "gdb",
+        miDebuggerServerAddress = "raspberrypi:3333",
+        miDebuggerPath = "/usr/bin/arm-none-eabi-gdb",
+        cwd = "${workspaceFolder}",
+        program = "${workspaceFolder}/build/" .. exec_name,
+
+        stopAtEntry = false,
+        postRemoteConnectCommands = {
+            {
+                text = "load build/" .. exec_name,
+            },
+            {
+                text = "monitor reset halt",
+            },
+            {
+                text = "break main",
+            },
+        },
+    }
+}
