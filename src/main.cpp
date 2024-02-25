240edaf553b4169a4ff7becc4d305953b57e09ab
Oskari Tuormaa <oskaritu@gmail.com>
2024-02-25 16:12:44 +0100

    Rename

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,35 @@
+#include <array>
 #include <pico/stdlib.h>
 
-#include "squarewave.pio.h"
+#include "n64-protocol.pio.h"
 
 static constexpr int gpio_num = 0;
 
-bool timer_callback(repeating_timer_t* t)
+bool timer_callback(repeating_timer_t* timer)
 {
-    PIO& pio = *static_cast<PIO*>(t->user_data);
-    pio_sm_put(pio, 0, 0x01 << 24U);
-    pio_sm_put(pio, 0, 0x12 << 24U);
+    constexpr unsigned               shift = 24U;
+    constexpr std::array<uint8_t, 1> data { 0x00 };
+
+    PIO& pio = *static_cast<PIO*>(timer->user_data);
+    for (const auto& var : data)
+    {
+        pio_sm_put(pio, 0, var << shift);
+    }
     return true;
 }
 
 int main()
 {
-    PIO  pio    = pio0;
-    uint sm     = 0;
-    uint offset = pio_add_program(pio, &squarewave_program);
+    // NOLINTNEXTLINE(*-cstyle-cast)
+    PIO  pio           = pio0;
+    uint state_machine = 0;
+    uint offset        = pio_add_program(pio, &n64_protocol_program);
 
-    pio_gpio_init(pio, gpio_num);
-    pio_sm_set_consecutive_pindirs(pio, sm, gpio_num, 1, true);
-
-    pio_sm_config c = squarewave_program_get_default_config(offset);
-    sm_config_set_sideset_pins(&c, gpio_num);
-    sm_config_set_out_shift(&c, false, true, 8);
-    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_TX);
-
-    sm_config_set_clkdiv(&c, 65.5F);
-
-    pio_sm_init(pio, sm, offset, &c);
-    pio_sm_set_enabled(pio, sm, true);
+    constexpr float freq = 250'000;
+    n64_protocol_program_init(pio, state_machine, offset, gpio_num, freq);
 
     repeating_timer_t timer;
-    add_repeating_timer_ms(-250, timer_callback, &pio, &timer);
+    add_repeating_timer_ms(-1, timer_callback, &pio, &timer);
 
     while (true) { }
 }
