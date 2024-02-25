6169849ff2ae4d8fff7341922608a293515569dc
Oskari Tuormaa <oskaritu@gmail.com>
2024-02-25 04:14:12 +0100

    Formatting

--- upload_and_program.sh
+++ upload_and_program.sh
@@ -2,4 +2,6 @@
 
 ELF_NAME=n64-sniffer.elf
 
-cmake --build build -j30 && scp build/${ELF_NAME} pi@raspberrypi:/home/pi && kitten ssh pi@raspberrypi "openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c \"program ${ELF_NAME} verify reset exit\""
+cmake --build build -j30 && \
+    scp build/${ELF_NAME} pi@raspberrypi:/home/pi && \
+    ssh pi@raspberrypi "openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c \"program ${ELF_NAME} verify reset exit\""
